c00c690eb83679018a774d720f6365a8e53785ad
nishi <pisces.nisikawa@gmail.com>
2018-10-06 04:52:54 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
+  protect_from_forgery with: :null_session
   before_action :skip_session
 
   protected
